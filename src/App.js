2d27a49c111a7dd5a952827bf64a9b7b89ab4e14
tangweikun <819573105@qq.com>
2018-08-15 11:03:23 +0800

    test prettier

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-heder">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
